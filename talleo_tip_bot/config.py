4a509612163490040d180006ba35ac1b305eb64e
Mika Lindqvist <postmaster@raasu.org>
2020-07-26 20:23:00 +0200

    Allow specifying configuration file on command line.

--- talleo_tip_bot/config.py
+++ talleo_tip_bot/config.py
@@ -2,15 +2,19 @@ import os
 
 import yaml
 from munch import Munch
-
-config_file_path = os.path.join(
-    os.path.dirname(__file__), os.path.pardir, 'config.yml')
+import argparse
+import sys
 
 config = None
 
 
 def load_config():
-    with open(config_file_path) as config_file:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', nargs='?', help='configuration file', default='../config.yml')
+    args, left = parser.parse_known_args()
+    sys.argv = sys.argv[:1]+left
+    print("Loading configuration file {}".format(args.config))
+    with open(args.config) as config_file:
         _config = yaml.load(config_file)
         globals()['config'] = Munch.fromDict(_config)
 
