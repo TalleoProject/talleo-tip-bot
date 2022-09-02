5bb15fc046e10a33fabf94a4beb0d17585ad0c96
Mika Lindqvist <postmaster@raasu.org>
2022-09-02 18:39:25 +0300

    Upgrade to discord.py 2.0.* * Require minimum of Python 3.8

--- gen_requirements.sh
+++ gen_requirements.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pipenv lock -r | grep -v '^\-i ' | cut -d ';' -f 1 - | cut -d ' ' -f 1 - > requirements.txt
-pipenv lock -r --dev | grep -v '^\-i ' | cut -d ';' -f 1 - | cut -d ' ' -f 1 - > requirements-dev.txt
+pipenv requirements | grep -v '^\-i ' | cut -d ';' -f 1 - | cut -d ' ' -f 1 - > requirements.txt
+pipenv requirements --dev | grep -v '^\-i ' | cut -d ';' -f 1 - | cut -d ' ' -f 1 - > requirements-dev.txt
